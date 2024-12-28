2eab1928300ca750864d68b1f3994f64c0e5b28d
Song Khoi <songkhoi123@gmail.com>
2024-12-28 22:25:01 +0700

    phase3

--- src/main.zig
+++ src/main.zig
@@ -6,15 +6,9 @@ pub fn main() !void {
 
     const stdout = io.getStdOut().writer();
 
-    try stdout.print("$ ", .{});
 
     const stdin = io.getStdIn().reader();
 
-    var buffer: [1024]u8 = undefined;
-
-    const user_input = try stdin.readUntilDelimiter(&buffer, '\n');
-
-    try stdout.print("{s}: command not found\n", .{user_input});
 
     while (true) {
 
