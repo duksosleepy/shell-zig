fae6d6145c2290df82204ca93a8be1dfd21a34af
Song Khoi <songkhoi123@gmail.com>
2024-12-31 10:14:26 +0700

    phase7

--- src/main.zig
+++ src/main.zig
@@ -1,95 +1,132 @@
 const std = @import("std");
 
-const stdout = std.io.getStdOut().writer();
+fn find_on_path(alloc: std.mem.Allocator, name: []const u8) !?[]const u8 {
 
-const stdin = std.io.getStdIn().reader();
+    const path = std.posix.getenv("PATH") orelse "";
 
-const btv = std.mem.bytesToValue;
+    var iter = std.mem.tokenizeScalar(u8, path, ':');
 
-const buildins = [_]*const [4:0]u8{ "exit", "echo", "type" };
+    while (iter.next()) |dirname| {
+
+        const joined = try std.fs.path.join(alloc, &[_][]const u8{ dirname, name });
+
+        if (std.fs.cwd().access(joined, std.fs.File.OpenFlags{})) {
+
+            return joined;
+
+        } else |_| {} // XXX probably nicer syntax for this?
+
+        alloc.free(joined);
+
+    }
+
+    return null;
+
+}
 
 pub fn main() !void {
 
-    try stdout.print("$ ", .{});
+    const stdout = std.io.getStdOut().writer();
+
+    const stdin = std.io.getStdIn().reader();
+
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+
+    const alloc = gpa.allocator();
 
     var buffer: [1024]u8 = undefined;
 
-    while (stdin.readUntilDelimiter(&buffer, '\n')) |line| {
+    while (true) {
 
-        const ret = try handleUserInput(line);
+        try stdout.print("$ ", .{});
 
-        if (ret == 0) {
+        const user_input_or_err = stdin.readUntilDelimiter(&buffer, '\n');
+
+        if (user_input_or_err == error.EndOfStream) {
+
+            try stdout.print("\n", .{});
 
             break;
 
         }
 
-        try stdout.print("$ ", .{});
+        const user_input = try user_input_or_err;
 
-    } else |err| {
+        var it = std.mem.splitScalar(u8, user_input, ' ');
 
-        try stdout.print("Error: {s}\n", .{@errorName(err)});
+        const cmd = it.first();
 
-    }
+        const rest = it.rest();
 
-}
+        if (std.mem.eql(u8, cmd, "exit")) {
 
-fn handleUserInput(line: []u8) !u8 {
+            var exit_code: u8 = 0;
 
-    if (line.len == 0) {
+            // bash prints an error and exits with 255 if the arg is not
 
-        return 1;
+            // an int, no matter how many arguments.
 
-    }
+            // zsh prints an error and refuses to exit if there is more than
 
-    var tokens = std.mem.splitSequence(u8, line, " ");
+            // one arg, but exits with 0 if the one arg is not an int.
 
-    const command = tokens.first();
+            // This just always exits with 0 if the number conversion fails.
 
-    const args = tokens.rest();
+            if (rest.len > 0)
 
-    switch (btv(u32, command)) {
+                exit_code = std.fmt.parseInt(u8, rest, 10) catch 0;
 
-        btv(u32, "exit") => return 0,
-
-        btv(u32, "echo") => {
-
-            try stdout.print("{s}\n", .{args});
-
-        },
-
-        btv(u32, "type") => {
-
-            try handleTypeBuitin(args);
-
-        },
-
-        else => {
-
-            try stdout.print("{s}: command not found\n", .{line});
-
-        },
-
-    }
-
-    return 1;
-
-}
-
-fn handleTypeBuitin(args: []const u8) !void {
-
-    for (buildins) |buildin| {
-
-        if (std.mem.eql(u8, buildin, args)) {
-
-            try stdout.print("{s} is a shell builtin\n", .{args});
-
-            return;
+            std.process.exit(exit_code);
 
         }
 
-    }
+        if (std.mem.eql(u8, cmd, "echo")) {
 
-    try stdout.print("{s}: not found\n", .{args});
+            try stdout.print("{s}\n", .{rest});
+
+            continue;
+
+        }
+
+        if (std.mem.eql(u8, cmd, "type")) {
+
+            if (std.mem.eql(u8, rest, "exit") or
+
+                std.mem.eql(u8, rest, "echo") or
+
+
+                std.mem.eql(u8, rest, "type"))
+
+            {
+
+                try stdout.print("{s} is a shell builtin\n", .{rest});
+
+            } else {
+
+                const path = try find_on_path(alloc, rest);
+
+                if (path) |p| {
+
+                    defer alloc.free(p);
+
+                    try stdout.print("{s} is {s}\n", .{ rest, p });
+
+                } else {
+
+                    try stdout.print("{s}: not found\n", .{rest});
+
+                }
+
+            }
+
+            continue;
+
+        }
+
+        // TODO: Handle user input
+
+        try stdout.print("{s}: not found\n", .{cmd});
+
+    }
 
 }
