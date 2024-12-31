3bb95b8ea0949ecbd10d54a5dd4e932294f8095c
Song Khoi <songkhoi123@gmail.com>
2024-12-31 12:00:29 +0700

    phase9

--- src/main.zig
+++ src/main.zig
@@ -1,155 +1,195 @@
 const std = @import("std");
 
-fn findBinPath(allocator: std.mem.Allocator, path: ?[]const u8, name: []const u8) !?[]const u8 {
+const process = std.process;
 
-    if (path) |p| {
+const stdout = std.io.getStdOut().writer();
 
-        var it = std.mem.tokenizeScalar(u8, p, ':');
+const mem = std.mem;
 
-        while (it.next()) |entry| {
+const fmt = std.fmt;
 
-            const bin = try std.fs.path.join(allocator, &.{ entry, name });
+const fs = std.fs;
 
-            std.fs.accessAbsolute(bin, .{}) catch continue;
+const allocator = std.heap.page_allocator;
 
-            return bin;
+const BuiltIn = enum {
+
+    echo,
+
+    exit,
+
+    type,
+
+    pwd,
+
+};
+
+const BuiltInHandler = *const fn (args: []const u8) anyerror!void;
+
+var handlers = std.AutoHashMap(BuiltIn, BuiltInHandler).init(allocator);
+
+pub fn main() !void {
+
+    var buffer: [1024]u8 = undefined;
+
+    defer handlers.deinit();
+
+    try handlers.put(.exit, handle_exit);
+
+    try handlers.put(.echo, handle_echo);
+
+    try handlers.put(.type, handle_type);
+
+    try handlers.put(.pwd, handle_pwd);
+
+    while (true) {
+
+        try stdout.print("$ ", .{});
+
+        const stdin = std.io.getStdIn().reader();
+
+        const user_input = try stdin.readUntilDelimiter(&buffer, '\n');
+
+        try handle_command(user_input);
+
+    }
+
+}
+
+fn handle_command(input: []const u8) !void {
+
+    var input_slices = mem.splitScalar(u8, input, ' ');
+
+    const cmd_str = input_slices.first();
+
+    const args = input_slices.rest();
+
+    const builtin = std.meta.stringToEnum(BuiltIn, cmd_str);
+
+    if (builtin) |bi| {
+
+        const handler = handlers.get(bi).?;
+
+        try handler(args);
+
+    } else {
+
+        try handle_default(cmd_str, args);
+
+    }
+
+}
+
+fn handle_echo(args: []const u8) !void {
+
+    try stdout.print("{s}\n", .{args});
+
+}
+
+fn handle_exit(args: []const u8) !void {
+
+    const exit_code = fmt.parseInt(u8, args, 10) catch 0;
+
+    process.exit(exit_code);
+
+}
+
+fn handle_type(args: []const u8) !void {
+
+    const builtin = std.meta.stringToEnum(BuiltIn, args);
+
+    if (builtin) |bi| {
+
+        try stdout.print("{s} is a shell builtin\n", .{@tagName(bi)});
+
+        return;
+
+    }
+
+    var arena = std.heap.ArenaAllocator.init(allocator);
+
+    defer arena.deinit();
+
+    const arena_alloc = arena.allocator();
+
+    if (try lookup_command(args, arena_alloc)) |path| {
+
+        try stdout.print("{s} is {s}\n", .{ args, path });
+
+    } else {
+
+        try stdout.print("{s}: not found\n", .{args});
+
+    }
+
+}
+
+fn handle_pwd(_: []const u8) !void {
+
+    const path = try fs.cwd().realpathAlloc(allocator, ".");
+
+    defer allocator.free(path);
+
+    try stdout.print("{s}\n", .{path});
+
+}
+
+fn handle_default(cmd: []const u8, args: []const u8) !void {
+
+    var arena = std.heap.ArenaAllocator.init(allocator);
+
+    defer arena.deinit();
+
+    const arena_alloc = arena.allocator();
+
+    const path = try lookup_command(cmd, arena_alloc) orelse {
+
+        try stdout.print("{s}: command not found\n", .{cmd});
+
+        return;
+
+    };
+
+    var argv = std.ArrayList([]const u8).init(arena_alloc);
+
+    try argv.append(path);
+
+    var args_iter = mem.splitScalar(u8, args, ' ');
+
+    while (args_iter.next()) |arg| {
+
+        try argv.append(arg);
+
+    }
+
+    var proc = process.Child.init(argv.items, arena_alloc);
+
+    _ = try proc.spawnAndWait();
+
+}
+
+fn lookup_command(cmd: []const u8, alloc: std.mem.Allocator) !?[]const u8 {
+
+    const path_var = try process.getEnvVarOwned(alloc, "PATH");
+
+    var path_iter = mem.splitScalar(u8, path_var, fs.path.delimiter);
+
+    while (path_iter.next()) |dir_path| {
+
+        const dir = fs.openDirAbsolute(dir_path, .{}) catch continue;
+
+        const file_stat = dir.statFile(cmd) catch continue;
+
+        if (file_stat.mode & std.c.S.IXUSR == 0) {
+
+            continue;
 
         }
+
+        return try fmt.allocPrint(alloc, "{s}{c}{s}", .{ dir_path, fs.path.sep, cmd });
 
     }
 
     return null;
-
-}
-
-pub fn main() !void {
-
-    const stdout = std.io.getStdOut().writer();
-
-    const stdin = std.io.getStdIn().reader();
-
-    var global_arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-
-    var loop_arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-
-    const allocator = loop_arena.allocator();
-
-    const env = try std.process.getEnvMap(global_arena.allocator());
-
-    const env_path = env.get("PATH");
-
-    while (true) {
-
-        _ = loop_arena.reset(.retain_capacity);
-
-        try stdout.print("$ ", .{});
-
-        var buffer: [1024]u8 = undefined;
-
-        const user_input = try stdin.readUntilDelimiter(&buffer, '\n');
-
-        const input = std.mem.trim(u8, user_input, &std.ascii.whitespace);
-
-        var args = std.ArrayList([]const u8).init(allocator);
-
-        var it = std.mem.tokenizeScalar(u8, input, ' ');
-
-        while (it.next()) |arg| {
-
-            try args.append(arg);
-
-        }
-
-        if (args.items.len > 0) {
-
-            const cmd = args.items[0];
-
-            if (std.mem.eql(u8, cmd, "exit")) {
-
-                var code: u8 = 0;
-
-                if (args.items.len > 1) code = try std.fmt.parseInt(u8, args.items[1], 10);
-
-                std.process.exit(code);
-
-            } else if (std.mem.eql(u8, cmd, "echo")) {
-
-                const slice = args.items[1..];
-
-                for (slice, 0..) |arg, i| {
-
-                    try stdout.print("{s}", .{arg});
-
-                    if (i != slice.len - 1) try stdout.print(" ", .{});
-
-                }
-
-                try stdout.print("\n", .{});
-
-            } else if (std.mem.eql(u8, cmd, "type")) {
-
-                if (args.items.len > 1) {
-
-                    const arg = args.items[1];
-
-                    if (std.mem.eql(u8, arg, "exit") or
-
-                        std.mem.eql(u8, arg, "echo") or
-
-                        std.mem.eql(u8, arg, "type"))
-
-                    {
-
-                        try stdout.print("{s} is a shell builtin\n", .{arg});
-
-                    } else if (try findBinPath(allocator, env_path, arg)) |bin| {
-
-                        try stdout.print("{s} is {s}\n", .{ arg, bin });
-
-                    } else {
-
-                        try stdout.print("{s}: not found\n", .{arg});
-
-                    }
-
-                }
-
-            } else if (try findBinPath(allocator, env_path, cmd)) |bin| {
-
-                const pid = try std.posix.fork();
-
-                if (pid == 0) {
-
-                    const bin_z = try allocator.dupeZ(u8, bin);
-
-                    var args_z = std.ArrayList(?[*:0]const u8).init(allocator);
-
-                    for (args.items) |arg| {
-
-                        try args_z.append(try allocator.dupeZ(u8, arg));
-
-                    }
-
-                    try args_z.append(null);
-
-                    const envp: [*:null]const ?[*:0]const u8 = &.{null};
-
-                    std.posix.execveZ(bin_z, @ptrCast(args_z.items.ptr), envp) catch {};
-
-                    std.process.exit(1);
-
-                }
-
-                _ = std.posix.waitpid(pid, 0);
-
-            } else {
-
-                try stdout.print("{s}: command not found\n", .{cmd});
-
-            }
-
-        }
-
-    }
 
 }
